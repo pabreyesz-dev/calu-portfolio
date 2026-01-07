b183876a7363452eb9520decdb5753ad84514045
pabreyesz-dev <pab.reyesz@gmail.com>
2026-01-06 23:17:29 -0300

    Update songs spotify
    half songs updated with images

--- src/lib/songs.ts
+++ src/lib/songs.ts
@@ -16,44 +16,104 @@ export interface Song {
 // El trackId es: 4uLU6hMCjMI75M1A2tKUQC
 export const songs: Song[] = [
   {
-    id: 'vas-a-ser-mio',
-    title: 'Vas a Ser Mío',
-    album: 'La Jugadora',
-    year: '2023',
-    duration: '3:45',
-    spotifyTrackId: '', // Agregar trackId aquí
-    spotifyUrl: 'https://open.spotify.com/track/...',
-    image: 'https://i.scdn.co/image/...' // URL de la portada
+    id: 'no-hay-compe',
+    title: 'No Hay Compe',
+    album: 'No Hay Compe',
+    year: '2025',
+    duration: '3:47',
+    spotifyTrackId: '74mBMRipJwTEFw1F25HhOI',
+    spotifyUrl: 'https://open.spotify.com/intl-es/track/74mBMRipJwTEFw1F25HhOI',
+    image: 'https://i.scdn.co/image/ab67616d00001e02c790411be379aa9b09243e48'
+  },
+  {
+    id: 'tu-nombre',
+    title: 'Tu nombre',
+    album: 'Tu nombre',
+    year: '2025',
+    duration: '3:05',
+    spotifyTrackId: '0meX9z4yBcgcHc41oVzoVH',
+    spotifyUrl: 'https://open.spotify.com/intl-es/track/0meX9z4yBcgcHc41oVzoVH',
+    image: 'https://i.scdn.co/image/ab67616d00001e023da21025624f662928071ad6'
+  },
+  {
+    id: 'la-ultima-vez',
+    title: 'La Última Vez',
+    album: 'La Última Vez',
+    year: '2024',
+    duration: '3:08',
+    spotifyTrackId: '5LrpIXuQrVzRJtSaHUNa8n',
+    spotifyUrl: 'https://open.spotify.com/intl-es/track/5LrpIXuQrVzRJtSaHUNa8n',
+    image: 'https://i.scdn.co/image/ab67616d00001e02e7166368691a662ece62a489'
   },
   {
     id: 'tu-gata',
     title: 'Tu Gata',
     album: 'Tu Gata',
     year: '2024',
-    duration: '3:20',
-    spotifyTrackId: '', // Agregar trackId aquí
-    spotifyUrl: 'https://open.spotify.com/track/...',
-    image: 'https://i.scdn.co/image/...'
+    duration: '2:41',
+    spotifyTrackId: '7IieYCEirGMEVvl2B6Ts3X',
+    spotifyUrl: 'https://open.spotify.com/intl-es/track/7IieYCEirGMEVvl2B6Ts3X',
+    image: 'https://i.scdn.co/image/ab67616d00001e0200a5519cb10fdba52f18bc2f'
   },
   {
-    id: 'sin-freno',
-    title: 'Sin Freno',
-    album: 'Sin Freno',
+    id: 'n4lgaje',
+    title: 'N4lgaje',
+    album: 'N4lgaje',
     year: '2024',
-    duration: '3:15',
-    spotifyTrackId: '', // Agregar trackId aquí
-    spotifyUrl: 'https://open.spotify.com/track/...',
-    image: 'https://i.scdn.co/image/...'
+    duration: '2:29',
+    spotifyTrackId: '0BDuDa2z9MVuXOPv3utWO6',
+    spotifyUrl: 'https://open.spotify.com/intl-es/track/0BDuDa2z9MVuXOPv3utWO6',
+    image: 'https://i.scdn.co/image/ab67616d00001e021df521e5047b3a1f2856c04c'
+  },
+  {
+    id: 'haciendo-oro',
+    title: 'Haciendo Oro',
+    album: 'Haciendo Oro',
+    year: '2023',
+    duration: '3:04',
+    spotifyTrackId: '5cdTl3FrzFrPaO9lORc7IG',
+    spotifyUrl: 'https://open.spotify.com/intl-es/track/5cdTl3FrzFrPaO9lORc7IG',
+    image: 'https://i.scdn.co/image/ab67616d00001e02e411e7883312990333e56d5e'
+  },
+  {
+    id: 'sere-tu-abrigo',
+    title: 'Seré Tu Abrigo',
+    album: 'Seré Tu Abrigo',
+    year: '2023',
+    duration: '3:19',
+    spotifyTrackId: '4awf8WjFJzJVmXdNU54T1m',
+    spotifyUrl: 'https://open.spotify.com/intl-es/track/4awf8WjFJzJVmXdNU54T1m',
+    image: 'https://i.scdn.co/image/ab67616d00001e027cda3481263694c797eae3df'
   },
   {
     id: 'la-jugadora',
     title: 'La Jugadora',
     album: 'La Jugadora',
     year: '2023',
-    duration: '3:30',
-    spotifyTrackId: '', // Agregar trackId aquí
-    spotifyUrl: 'https://open.spotify.com/track/...',
-    image: 'https://i.scdn.co/image/...'
+    duration: '3:26',
+    spotifyTrackId: '2L6rUsJjuYsa8oSwtEVTAN',
+    spotifyUrl: 'https://open.spotify.com/intl-es/track/2L6rUsJjuYsa8oSwtEVTAN',
+    image: 'https://i.scdn.co/image/ab67616d00001e02e8d8e451b738b3d586f2f216'
+  },
+  {
+    id: 'turn-off-the-lights',
+    title: 'Turn off The Lights',
+    album: 'Turn off The Lights',
+    year: '2023',
+    duration: '4:13',
+    spotifyTrackId: '7wP3v4C9ORKbR8YW2niKQ1',
+    spotifyUrl: 'https://open.spotify.com/intl-es/track/7wP3v4C9ORKbR8YW2niKQ1',
+    image: 'https://i.scdn.co/image/ab67616d00001e0208220551594969d1cde173e6'
+  },
+  {
+    id: 'sin-freno',
+    title: 'Sin Freno',
+    album: 'Sin Freno',
+    year: '2023',
+    duration: '3:03',
+    spotifyTrackId: '5KRNjsXj8LIEn8d5dqdWk5',
+    spotifyUrl: 'https://open.spotify.com/intl-es/track/5KRNjsXj8LIEn8d5dqdWk5',
+    image: 'https://i.scdn.co/image/ab67616d00001e025bee207d9da5ad6af1214921'
   },
   {
     id: 'libre-por-la-ciudad',
