85a78b1c6d6b06dce0bcf4fe5c107ee256646f95
pabreyesz-dev <pab.reyesz@gmail.com>
2026-01-10 02:02:05 -0300

    Update youtube songs
    update typescript file for the direct linked youtube songs

--- src/lib/youtube.ts
+++ src/lib/youtube.ts
@@ -12,25 +12,67 @@ export interface YouTubeVideo {
 // Para obtener el videoId: de la URL https://www.youtube.com/watch?v=VIDEO_ID, copia el VIDEO_ID
 export const youtubeVideos: YouTubeVideo[] = [
   {
-    id: 'video-1',
-    title: 'Video 1',
-    description: 'Descripción del video',
-    videoId: '', // Reemplazar con el ID del video
+    id: 'pop-video',
+    title: 'POP',
+    description: 'VIDEOCLIP OFICIAL POP',
+    videoId: 'm77YIiZbPMI',
     publishedAt: '2025'
   },
   {
-    id: 'video-2',
-    title: 'Video 2',
-    description: 'Descripción del video',
-    videoId: '', // Reemplazar con el ID del video
+    id: 'instagram',
+    title: 'INSTAGRAM',
+    description: 'VIDEOCLIP OFICIAL INSTAGRAM',
+    videoId: '3yzUR4OBeS8',
+    publishedAt: '2025'
+  },
+  {
+    id: 'vas a ser mio',
+    title: 'VAS A SER MÍO',
+    description: 'VIDEOCLIP OFICIAL VAS A SER MÍO',
+    videoId: 'euG63JvOaxs',
     publishedAt: '2024'
   },
   {
-    id: 'video-3',
-    title: 'Video 3',
-    description: 'Descripción del video',
-    videoId: '', // Reemplazar con el ID del video
-    publishedAt: '2024'
+    id: 'violenta',
+    title: 'VIOLENTA',
+    description: 'VIDEOCLIP OFICIAL VIOLENTA',
+    videoId: 'PGA1hJDP4pA',
+    publishedAt: '2023'
+  },
+  {
+    id: 'dependencia',
+    title: 'DEPENDENCIA',
+    description: 'VIDEOCLIP OFICIAL DEPENDENCIA',
+    videoId: 'rlFMWKYDevM',
+    publishedAt: '2023'
+  },
+  {
+    id: 'no puedo obligarte',
+    title: 'NO PUEDO OBLIGARTE',
+    description: 'VIDEOCLIP OFICIAL NO PUEDO OBLIGARTE',
+    videoId: 'vGXtNImQYtI',
+    publishedAt: '2023'
+  },
+  {
+    id: 'ritual',
+    title: 'RITUAL',
+    description: 'VIDEOCLIP OFICIAL RITUAL',
+    videoId: 'cfyZZt7sfH0',
+    publishedAt: '2022'
+  },
+  {
+    id: 'irma',
+    title: 'IRMA',
+    description: 'VIDEOLYRIC OFICIAL IRMA',
+    videoId: 'EeCKE9kkc3A',
+    publishedAt: '2022'
+  },
+  {
+    id: 'castillo',
+    title: 'CASTILLO',
+    description: 'VIDEOCLIP OFICIAL CASTILLO',
+    videoId: 'TQ0029Z9gno',
+    publishedAt: '2022'
   }
 ];
 
